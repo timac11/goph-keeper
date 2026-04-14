dff4098089aac6521b8441bdf65e2433eba5fe82
aleksandrsokolov <timac11@bk.ru>
2026-04-14 09:13:09 +0300

    add user repository

--- go.mod
+++ go.mod
@@ -5,7 +5,10 @@ go 1.25.1
 require github.com/jackc/pgx/v5 v5.9.1
 
 require (
+	github.com/Masterminds/squirrel v1.5.4 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 )
@@ -14,6 +17,7 @@ require (
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/golang-jwt/jwt/v4 v4.5.2
 	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
---
+++ internal/server/repository/user.go
@@ -0,0 +1,81 @@
+package repository
+
+import (
+	"context"
+	internalErrors "errors"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+
+	"github.com/timac11/goph-keeper/internal/common/model"
+	"github.com/timac11/goph-keeper/internal/server/errors"
+)
+
+func (client *PgClient) CreateUser(ctx context.Context, user *model.UserLoginDto) (*model.User, error) {
+	var userModel model.User
+
+	query, _, err := sq.Insert("user").
+		Columns("login", "password").
+		Values(user.Login, user.Password).
+		Suffix("RETURNING id, login, password").
+		ToSql()
+
+	err = client.pool.
+		QueryRow(ctx, query).
+		Scan(&userModel.ID, &userModel.Login, &userModel.Password)
+
+	if err != nil {
+		if pgErr, ok := err.(*pgconn.PgError); ok && pgerrcode.IsIntegrityConstraintViolation(pgErr.Code) {
+			return nil, errors.NewEntityError(err, errors.EntityAlreadyExists, user)
+		}
+		return nil, err
+	}
+
+	return &userModel, nil
+}
+
+func (client *PgClient) GetUserByLogin(ctx context.Context, login string) (*model.User, error) {
+	var user model.User
+
+	query, _, err := sq.Select("id", "login", "password").
+		From("user").
+		Where(sq.Eq{"login": login}).
+		ToSql()
+
+	err = client.pool.
+		QueryRow(ctx, query).
+		Scan(&user.ID, &user.Login, &user.Password)
+
+	if err != nil {
+		if internalErrors.Is(err, pgx.ErrNoRows) {
+			return nil, errors.NewEntityError(err, errors.EntityNotFound, login)
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (client *PgClient) GetUserByID(ctx context.Context, id string) (*model.User, error) {
+	var user model.User
+
+	query, _, err := sq.Select("id", "login", "password").
+		From("user").
+		Where(sq.Eq{"id": id}).
+		ToSql()
+
+	err = client.pool.
+		QueryRow(ctx, query).
+		Scan(&user.ID, &user.Login, &user.Password)
+
+	if err != nil {
+		if internalErrors.Is(err, pgx.ErrNoRows) {
+			return nil, errors.NewEntityError(err, errors.EntityNotFound, id)
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
